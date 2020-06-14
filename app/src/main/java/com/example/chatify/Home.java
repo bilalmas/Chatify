4f04a6db816bcb042e8cade3c0da88a6ea2a3c42
Bilal Masood <49684325+bilalmas@users.noreply.github.com>
2020-06-14 23:21:25 +0500

    Weather Widget Created in Home Activity

--- app/src/main/java/com/example/chatify/Home.java
+++ app/src/main/java/com/example/chatify/Home.java
@@ -6,9 +6,18 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -17,12 +26,15 @@ public class Home extends AppCompatActivity {
     private Date currentTime;
     private TextView textViewTime;
     private BroadcastReceiver minuteUpdateReceiver;
+    private TextView weatherTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         textViewTime = (TextView)findViewById(R.id.textViewTime);
+        weatherTextView = (TextView) findViewById(R.id.weatherTextView);
+
        // currentTime = Calendar.getInstance().getTime();
         //textViewTime.setText(currentTime.toString());
     }
@@ -45,6 +57,58 @@ public class Home extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         startTimeUpdater();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(minuteUpdateReceiver);
+    }
+
+    public class WeatherTask extends AsyncTask<String,Void,String>{
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String result="";
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try {
+                url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+                int data = reader.read();
+                while(data != -1){
+
+                    char current = (char) data;
+                    result += current;
+                    data = reader.read();
+                }
+                return result;
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return result;
+
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            //Log.i("Website content :",result);
+            weatherTextView.setText(result);
+
+        }
+    }
+
+    public void weatherRefresh(View view){
+        WeatherTask task = new WeatherTask();
+        task.execute("http://api.openweathermap.org/data/2.5/weather?q=Lahore&appid=dc31b5af70b15eebcd73af4d546e1e1b");
+
     }
 
 
